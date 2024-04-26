020a5456d25276f149d54c4c63f544828d60c07d
Rémy Maillard <remy85603@gmail.com>
2024-04-26 06:40:17 +0300

    MEDIUM: ci: Added a simple HUD

---
+++ Assets/Scripts/HUD.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUD : MonoBehaviour
+{    public GameObject Gun;
+    public GameObject Bullets;
+    public GameObject GunOB;
+
+    public GameObject Shotgun;
+    public GameObject Shells;
+    public GameObject ShotgunOB;
+
+
+
+
+    void Start()
+    {
+
+    }
+
+
+
+
+    void Update()
+    {
+
+        if (GunOB.activeInHierarchy)
+        {
+            Gun.SetActive(true);
+            Bullets.SetActive(true);
+        }
+
+        else
+        {
+            Gun.SetActive(false);
+            Bullets.SetActive(false);
+        }
+
+        if (ShotgunOB.activeInHierarchy)
+        {
+            Shotgun.SetActive(true);
+            Shells.SetActive(true);
+        }
+
+        else
+        {
+            Shotgun.SetActive(false);
+            Shells.SetActive(false);
+        }
+    }
+}
