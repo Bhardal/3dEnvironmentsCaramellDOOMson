24da2ca9ea862f30014621e29c0dc3825990a97a
Rémy Maillard <remy85603@gmail.com>
2024-04-27 08:43:57 +0300

    MEDIUM: ci: Added a working healthbar and shieldbar

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public RawImage healthBar;
+    public RawImage shieldBar;
+    public float CurrentHealth;
+    public float CurrentShield;
+    private float MaxHealth = 100f;
+    private float MaxShield = 50f;
+    public Texture2D[] HealthBarImages;
+    public Texture2D[] ShieldBarImages;
+    PlayerHealth player;
+    private int healthNumber;
+    private int shieldNumber;
+
+
+
+
+
+    void Start()
+    {
+        // healthBar = GetComponent<RawImage>();
+        // shieldBar = GetComponent<RawImage>();
+        player = FindObjectOfType<PlayerHealth>();
+
+    }
+
+
+
+    void Update()
+    {
+        CurrentHealth = player.health;
+        CurrentShield = player.shield;
+        healthNumber = (int)Math.Round(CurrentHealth / 5);
+        if (healthNumber < 0)
+        {
+            healthNumber = 0;
+        }
+        shieldNumber = (int)Math.Round(CurrentShield / 5);
+        if (shieldNumber < 0)
+        {
+            shieldNumber = 0;
+        }
+        healthBar.texture = HealthBarImages[healthNumber];
+        shieldBar.texture = ShieldBarImages[shieldNumber];
+
+
+
+    }
+}
